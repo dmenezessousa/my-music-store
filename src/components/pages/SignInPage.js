06704bfb1d013ddb87eec1727fbb5be8291b52b6
Diego Sousa <khesanh@diegos-mbp.myfiosgateway.com>
2022-02-23 07:32:32 -0500

    added user to reduxStore

--- src/components/pages/SignInPage.js
+++ src/components/pages/SignInPage.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import { fetchUser } from '../../fetchData';
+import React ,{useState}from 'react';
+import {useDispatch, useSelector} from "react-redux"
+
 import Layout from '../layout/Layout';
+import { fetchUser } from '../../fetchData';
+import { userLogIn } from '../../reduxStore';
+
+
+
+
+const Form = () =>{
+
+  const [{email,password}, setCredentials] = useState({email:"",password: ""});
+  const dispatch = useDispatch()
+
+  const onSubmit = () =>{
+
+    fetchUser(email,password)
+    .then((user)=>dispatch(userLogIn(user)))
+    .catch((error)=>{
+      console.log("error", error);
+    });
+    
+  };
+
+  return(
+    <div>
+    <input 
+        type="email" 
+        label="Email"
+        value={email}
+        onChange={(event)=>{
+          setCredentials({password, email: event.target.value})
+        }}
+    ></input>
+    <input 
+        type="text" 
+        label="Password"
+        value={password}
+        onChange={(event)=>{
+          setCredentials({email, password: event.target.value})
+        }}
+    ></input>
+    <button onClick={onSubmit}>Log in</button>
+  </div>
+  )
+}
+
+
 
 const SignInPage = () => {
 
-  const handleUserLogIn = () => fetchUser('fake@user.com', '123')
-  //Put the data on redux
-  .then((data) => console.log('data: ', data));
+const user = useSelector((state)=>state.user);
 
-  return (
-    <Layout>
-      <h1>Sign In page</h1>
-      <button onClick={handleUserLogIn}>Log in user</button>
+ return(
+     <Layout>
+      <h3>{user ? `Welcome Back ${user.name}` : <Form/>}</h3>
     </Layout>
-  )
+ )
 };
 
 export default SignInPage;
--- src/reduxStore.js
+++ src/reduxStore.js
@@ -1,52 +1,74 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-const shoppingCartInitialState = [];
+// const shoppingCartInitialState = [];
 
-export const addItemToCartAction = 'addItemToCart';
+// export const addItemToCartAction = 'addItemToCart';
 
-export const removeItemFromCartAction = 'removeItemFromCart';
+// export const removeItemFromCartAction = 'removeItemFromCart';
 
-export const emptyCartAction = 'emptyCart';
+// export const emptyCartAction = 'emptyCart';
 
-const shoppingCartReducer = (state = shoppingCartInitialState, action) => {
+// const shoppingCartReducer = (state = shoppingCartInitialState, action) => {
 
-  if(action.type === addItemToCartAction){
-    // write some logic to add item to cart
+//   if(action.type === addItemToCartAction){
+//     // write some logic to add item to cart
 
-    // Check if the item is already in the cart
-    const itemFoundInCart = state.find(cartItem => cartItem.id === action.cartItem.id);
+//     // Check if the item is already in the cart
+//     const itemFoundInCart = state.find(cartItem => cartItem.id === action.cartItem.id);
 
-    // if its not, we can add it at the end with a quantity of 1
-    if(!itemFoundInCart){
-      return [...state, {...action.cartItem, quantity: 1}];
-    }
+//     // if its not, we can add it at the end with a quantity of 1
+//     if(!itemFoundInCart){
+//       return [...state, {...action.cartItem, quantity: 1}];
+//     }
 
-    // if it is actually already in the cart    
-    // we will still remove it
-    const cartWithFoundItemRemoved = state.filter(item => item.id !== action.cartItem.id);
-    // but add it again with the correct updated quantity.
-    return [...cartWithFoundItemRemoved, {...action.cartItem, quantity: itemFoundInCart.quantity + 1 }]
+//     // if it is actually already in the cart    
+//     // we will still remove it
+//     const cartWithFoundItemRemoved = state.filter(item => item.id !== action.cartItem.id);
+//     // but add it again with the correct updated quantity.
+//     return [...cartWithFoundItemRemoved, {...action.cartItem, quantity: itemFoundInCart.quantity + 1 }]
     
+//   }
+
+//   if(action.type === removeItemFromCartAction){
+//     // write some logic to remove item fromn cart
+
+//     return state.filter(item => item.id !== action.itemId)
+//   }
+
+
+//   if(action.type === emptyCartAction){
+//     // write some logic to empty the shopping cart
+//     return shoppingCartInitialState;
+//   }
+
+//   return state;
+// };
+
+const ACTIONS = {
+  LOG_IN: "sign-in",
+  USERSTATE: {user: undefined},
+}
+
+export const userLogIn = (user)=>({
+  type: ACTIONS.LOG_IN,
+  payload: {user: user},
+})
+
+const userReducer = (state , action) =>{
+  switch (type.action) {
+    case ACTIONS.LOG_IN:
+          const {payload} = action;
+          return {...state, user: payload.user} 
+    default:
+      return state;      
   }
-
-  if(action.type === removeItemFromCartAction){
-    // write some logic to remove item fromn cart
-
-    return state.filter(item => item.id !== action.itemId)
-  }
-
-
-  if(action.type === emptyCartAction){
-    // write some logic to empty the shopping cart
-    return shoppingCartInitialState;
-  }
-
-  return state;
-};
+}
 
 const store = configureStore({
   reducer: {
-    shoppingCart:  shoppingCartReducer,
+    // shoppingCart:  shoppingCartReducer,
+    userReducer,
+    userState: ACTIONS.USERSTATE,
   },
 })
 
